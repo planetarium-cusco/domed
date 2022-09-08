77c432474f5fc826713b8c2afb9d147cf014fd25
renatmc <lordace550@gmail.com>
2022-09-07 21:01:39 -0500

    feat: add planes to sphere

--- src/components/ui/Sphere/temp.tsx
+++ src/components/ui/Sphere/temp.tsx
@@ -1,11 +1,19 @@
 import { Position } from "@react-three/drei/helpers/Position";
 import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
-import { JSXElementConstructor, ReactElement, ReactFragment, useEffect, useMemo, useRef } from "react";
+import {
+  JSXElementConstructor,
+  ReactElement,
+  ReactFragment,
+  useEffect,
+  useMemo,
+  useRef,
+} from "react";
 import {
   BackSide,
   DoubleSide,
   Mesh,
   MeshBasicMaterial,
+  Object3D,
   ObjectLoader,
   PlaneGeometry,
   TextureLoader,
@@ -15,89 +23,57 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { generateUUID } from "three/src/math/MathUtils";
 
 export default function App() {
-const planeslist: any[] = [];
-//const vv = new Vector3(3, 2, 3);
-
-type TilesProps ={
-  position: Vector3;
-}
-
-  function Plane(props: TilesProps) {
-    const planeRef2 = useRef<Mesh>(null!);
-
-    planeRef2.current?.position.copy(props.position);
-    planeRef2.current?.lookAt(0,0,0);
-    return (
-      <mesh ref={planeRef2} dispose={null}>
-
-          <planeGeometry args={[0.1, 0.1, 16, 16]} />
-
-          <meshStandardMaterial color={0xffffff} side={DoubleSide} />
-        </mesh>
-    );
-  }
-
   function Sphere() {
     const sphereRef = useRef<Mesh>(null!);
 
+    const planeRef = useRef(null!);
+
+    const planeGeometry = new PlaneGeometry(0.1, 0.1, 16, 16);
+    const planeMaterial = new MeshBasicMaterial({
+      color: 0xffffff,
+      side: DoubleSide,
+    });
+
+    const tempPlanes = new Object3D();
 
     useEffect(() => {
-      const vertices = sphereRef.current.geometry.attributes.position
-        .array as Array<number>;
+      let counter = 0;
+      const vertices = sphereRef.current.geometry.attributes.position.array as Array<number>;
 
       const length = vertices.length;
 
-      // const lookDirection = new Vector3();
-      // const target = new Vector3();
-      var count= 0;
+      const lookDirection = new Vector3();
+      const target = new Vector3();
+
       for (let i = 0; i < length; i += 3) {
-          const v = new Vector3(vertices[i], vertices[i + 1], vertices[i + 2]);
-          planeslist.push(
-            v
-          //<Plane key={count} position ={v}/>  
-          );
-          //count ++;
-          //planeRef.current.position.copy(v);
-          
-          //console.log(planeRef.current.position);
-          // lookDirection
-          //   .subVectors(planeRef.current.position, sphereRef.current.position)
-          //   .normalize();
-        
-        
+        const id = (counter += 3);
+        const v = new Vector3(vertices[i], vertices[i + 1], vertices[i + 2]);
+
+        tempPlanes.position.copy(v);
+        console.log(tempPlanes.position);
+
+        tempPlanes.updateMatrix();
+
+        planeRef.current.setMatrixAt(id, tempPlanes.matrix);
+
+        lookDirection.subVectors(tempPlanes.position, sphereRef.current.position).normalize();
+        target.copy(tempPlanes.position).add(lookDirection);
+        tempPlanes.lookAt(target);
       }
-      //const planecount = planespos.length;
-    });
+
+      planeRef.current.instanceMatrix.needsUpdate = true;
+    }, []);
 
     return (
       <group>
         <mesh ref={sphereRef}>
           <sphereGeometry args={[1, 50, 25]} />
-
-          <meshStandardMaterial
-            color={0xa9a9a9}
-            wireframe={true}
-            // side={BackSide}
-          />
-          
-          
-
+          <meshStandardMaterial color={0xa9a9a9} wireframe={true} />
         </mesh>
-        {planeslist.map((_,index)=> 
-          
-          <Plane position ={planeslist[index]}/>
-          )
-          }
-
-        
-        
+        <instancedMesh ref={planeRef} args={[planeGeometry, planeMaterial, 3000]} />
       </group>
     );
   }
-
-
-
-  
 
   function Controls() {
     const { camera, gl } = useThree();
@@ -115,31 +91,22 @@ type TilesProps ={
     return (
       <mesh ref={floorRef} position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
         <circleBufferGeometry args={[1, 50]} />
-        <meshStandardMaterial
-          color={0xa9a9a9}
-          wireframe={true}
-          side={DoubleSide}
-        />
+        <meshStandardMaterial color={0xa9a9a9} wireframe={true} side={DoubleSide} />
       </mesh>
     );
   }
 
-
   return (
-    <div id="canvas-container" style={{ width: "100vw", height: "100vh" }}> 
+    <div id="canvas-container" style={{ width: "100vw", height: "100vh" }}>
       <Canvas
         camera={{
           fov: 60,
           far: 100,
-          position: [0, 0, 4], 
+          position: [0, 0, 4],
         }}
       >
         <color attach="background" args={[0x090c17]} />
-        <hemisphereLight
-          color={0xffffff}
-          groundColor={0xbbbbff}
-          intensity={0.3}
-        />
+        <hemisphereLight color={0xffffff} groundColor={0xbbbbff} intensity={0.3} />
         <directionalLight position={[0.2, 1, 1]} />
         <Sphere />
         <Floor />
