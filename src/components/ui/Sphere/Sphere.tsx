4290551ab1a9247f8b9cfe9091111f176eecd035
Bregy Malpartida <bregy@minsky.cc>
2022-09-13 22:27:59 -0500

    proposal: a new hope

--- src/components/ui/Sphere/Sphere.tsx
+++ src/components/ui/Sphere/Sphere.tsx
@@ -1,49 +1,37 @@
 import { SphereGeometry } from "three";
 import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
-import {
-  JSXElementConstructor,
-  ReactElement,
-  ReactFragment,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
-import {
-  BackSide,
-  FrontSide,
-  DoubleSide,
-  Mesh,
-  MeshBasicMaterial,
-  Object3D,
-  ObjectLoader,
-  PlaneGeometry,
-  TextureLoader,
-  Vector3,
-} from "three";
+import { useEffect, useRef, useState } from "react";
+import { DoubleSide, Mesh, Vector3 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { generateUUID } from "three/src/math/MathUtils";
 
 type SphereMeshProps = {
-  radius: number;
+  radius?: number;
+  widthSegments?: number;
+  heightSegments?: number;
 };
 
-export default function SphereModel(props: SphereMeshProps) {
+export default function SphereModel({
+  radius = 20,
+  widthSegments = Math.ceil(radius / 2),
+  heightSegments = Math.floor(radius / 2),
+}: SphereMeshProps) {
   const sphereRef = useRef<Mesh | null>(null);
-  const geometryRef = useRef<Mesh | null>(null);
+  // const geometryRef = useRef<Mesh | null>(null);
 
-  // const planeRef = useRef(null!);
-  const texture = new TextureLoader().load(
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Starsinthesky.jpg/1920px-Starsinthesky.jpg"
-  );
-  const planeGeometry = new PlaneGeometry(0.13 * props.radius, 0.13 * props.radius);
-  planeGeometry.rotateY(0.13);
-  const planeMaterial = new MeshBasicMaterial({
-    map: texture,
-    side: DoubleSide,
-    color: 0x999999,
-  });
-  const [vertices, setVertices] = useState<Vector3[] | null>(null);
+  // const [vertices, setVertices] = useState<Vector3[] | null>(null);
+  const [planes, setPlanes] = useState<Array<Vector3[]> | null>(null);
+
+  const vector3ArrayToFloat32Array = (vectors: Vector3[]): Float32Array => {
+    const array = new Float32Array(vectors.length * 3);
+    vectors.forEach((vector, index) => {
+      array[index * 3] = vector.x;
+      array[index * 3 + 1] = vector.y;
+      array[index * 3 + 2] = vector.z;
+    });
+
+    return array;
+  };
 
   useEffect(() => {
     const sphereVertices = sphereRef?.current?.geometry?.attributes?.position?.array;
@@ -60,47 +48,65 @@ export default function SphereModel(props: SphereMeshProps) {
         return new Vector3(sphereVertices[id], sphereVertices[id + 1], sphereVertices[id + 2]);
       });
 
-    setVertices(vertices);
-  }, []);
+    // vertices.filter((vert, i) => i === 0);
+    const planes: Array<Vector3[]> = [];
 
-  const vector3ArrayToFloat32Array = (vectors: Vector3[]): Float32Array => {
-    const array = new Float32Array(vectors.length * 3);
-    vectors.forEach((vector, index) => {
-      array[index * 3] = vector.x;
-      array[index * 3 + 1] = vector.y;
-      array[index * 3 + 2] = vector.z;
+    vertices.forEach((vert, x) => {
+      const v1 = vertices[x];
+      const v2 = vertices[x + 1];
+      const v3 = vertices[x + widthSegments + 1];
+      const v4 = vertices[x + widthSegments];
+
+      if (v1 && v2 && v3 && v4) {
+        planes.push([v1, v2, v3, v4]);
+      }
     });
 
-    return array;
-  };
+    // setVertices(vertices);
+    setPlanes(planes);
+
+    console.log(planes);
+    //.slice(widthSegments, widthSegments + 3)
+  }, []);
 
   useEffect(() => {
-    if (vertices) {
-      console.log(vector3ArrayToFloat32Array(vertices));
-      console.log(geometryRef);
+    if (planes) {
+      // vertices
+      //   .filter((_, i) => i > 51)
+      //   .map((vertex, j) => {
+      //     // vertices[]
+      //   });
+
+      console.log("planes", planes);
+      console.log(vector3ArrayToFloat32Array(planes[0]));
     }
-  }, [vertices]);
+  }, [planes]);
 
   return (
     <group>
       <mesh ref={sphereRef} visible={false}>
-        <sphereGeometry args={[props.radius, 50, 25]} />
+        <sphereGeometry args={[radius, widthSegments, heightSegments]} />
         {/* <meshStandardMaterial color={0xa9a9a9} wireframe={true} /> */}
       </mesh>
       {/* <instancedMesh ref={planeRef} args={[planeGeometry, planeMaterial, 1987]} /> */}
-      {vertices && (
-        <mesh ref={geometryRef}>
-          <bufferGeometry>
-            <bufferAttribute
-              attach="attributes-position"
-              array={vector3ArrayToFloat32Array(vertices)}
-              count={vertices.length}
-              itemSize={3}
-            />
-          </bufferGeometry>
-          <meshBasicMaterial attach="material" color={0xff0000} />
-        </mesh>
-      )}
+      {planes &&
+        planes.map((plane, i) => {
+          console.log("plane", plane);
+          return (
+            <mesh key={i}>
+              <bufferGeometry>
+                <bufferAttribute
+                  attach="attributes-position"
+                  array={vector3ArrayToFloat32Array(plane)}
+                  count={plane.length}
+                  itemSize={3}
+                />
+              </bufferGeometry>
+              <meshBasicMaterial attach="material" color={0xff0000} side={DoubleSide} />
+              {/* <meshStandardMaterial attach="material" color={0xff0000} /> */}
+            </mesh>
+          );
+        })}
     </group>
   );
 }
--- src/modules/home/HomePageSphere.tsx
+++ src/modules/home/HomePageSphere.tsx
@@ -1,28 +1,25 @@
-import { SphereModel } from "components/ui/Sphere/Sphere"
+import { SphereModel } from "components/ui/Sphere/Sphere";
 import CanvasComponent from "components/ui/Canvas";
 import FloorModel from "components/ui/Sphere/Floor";
-import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
-import { Canvas } from '@react-three/fiber'
+import { VRButton, ARButton, XR, Controllers, Hands } from "@react-three/xr";
+import { Canvas } from "@react-three/fiber";
 
-const Radius = 50;
+// const Radius = 50;
 
 const HomePageContentSphere = () => {
   return (
     <>
-  <VRButton />
-  <CanvasComponent>
-    <XR>
-    <Controllers />
-    <Hands />
-    
-  </XR>
-      <SphereModel  radius={Radius} />
-      <FloorModel radius={Radius}/>
-  </CanvasComponent>
+      <VRButton />
+      <CanvasComponent>
+        <XR>
+          <Controllers />
+          <Hands />
+        </XR>
+        <SphereModel />
+        {/* <FloorModel radius={Radius}/> */}
+      </CanvasComponent>
     </>
   );
 };
 
 export { HomePageContentSphere };
-
-
